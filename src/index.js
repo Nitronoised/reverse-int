8b34f141560875145e34dd04f2d4a773671d2532
Yaseen Al-Hashimi <55101943+Nitronoised@users.noreply.github.com>
2021-02-18 11:07:37 +0200

    init : solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+  let revIndex = [];
+  let result = [];
+  let num = n.toString().split('');
+  for (let i = 0; i < num.length; i++) {
+    revIndex.push(i)
+  }
+  revIndex = revIndex.sort((a, b) => b - a);
+  for (let i = 0; i < revIndex.length; i++) {
+    result.push(num[revIndex[i]])
+  }
+  return parseInt(result.join(''))
 }
